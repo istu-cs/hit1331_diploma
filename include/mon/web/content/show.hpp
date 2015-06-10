b259eab11f6bbcee21512c20fd755eeab41c0284
Aleksey Filippov <sarum9in@gmail.com>
2015-06-10 16:11:27 +0400

    Implemented show model and view.

--- include/mon/web/content/show.hpp
+++ include/mon/web/content/show.hpp
@@ -1,12 +1,29 @@
 #pragma once
 
+#include <mon/server.pb.h>
 #include <mon/web/content/base_form.hpp>
+
+#include <string>
+#include <vector>
+#include <unordered_map>
 
 namespace mon {
 namespace web {
 namespace content {
 
-struct show : base_form {};
+struct show : base_form {
+    struct agent {
+        std::string name;
+        std::string target;
+        std::unordered_map<std::string, CheckResponse> stats;
+    };
+    struct plugin {
+        std::string id;
+        std::string name;
+    };
+    std::vector<agent> agents;
+    std::vector<plugin> plugins;
+};
 
 }  // namespace content
 }  // namespace web
